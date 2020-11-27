fc3f2733a5f228867c47750f41befd0141e25d24
stivgo <stiveng0630@gmail.com>
2020-11-27 14:11:46 -0500

    Fotos inmuebles busqueda

--- buenvecino/src/Vista/components/InmuebleMapa.js
+++ buenvecino/src/Vista/components/InmuebleMapa.js
@@ -1,13 +1,14 @@
 import React,{Component} from 'react';
 import '../styles/InmuebleMapa.css';
 import img from '../assets/Rectangle7.png'
+import ManejadorSg from '../../Modelo/Firebase/ManjadorSg';
 
 
 class InmuebleMapa extends Component{
     render(){
         return (
             <div className="InmuebleMapa">
-                <img src={img} alt=""/>
+                <img src={this.state.imagen} alt=""/>
                 <div>
                     <h3 className="Titulo">{this.props.info.nombre}</h3>
                     <p>{this.props.info.descripcion}</p>
@@ -17,8 +18,21 @@ class InmuebleMapa extends Component{
         );
     }
 
-    constructor(props){
+     constructor(props){
         super()
+        console.log(props.info)
+        this.state = {
+             imagen:{img}
+          }
+        
+    }
+    async componentDidMount(){
+        let imagenes = await ManejadorSg.obtenerFotosInmueble(this.props.info.idFirebase)
+        if(imagenes.length !== 0){
+            console.log("entro funcion")
+            await this.setState({imagen:imagenes[0]})
+        }
+        
     }
 }
 
